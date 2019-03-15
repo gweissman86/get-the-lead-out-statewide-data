c195c2a38320b91c84cfdcae6f95cbdc413604a6
Sadie Gill <thesadie@gmail.com>
2019-03-15 14:21:54 -0700

    joins datasets

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Data Dictionary
+district: name of district
+schoolName: name of school     
+schoolAddress: school address  
+medianResult: median lead found for test above 5 ppb
+unit: unit of median result (ppb = parts per billion)
+lead: was lead detected above 5 ppb (TRUE, FALSE, or NA if not tested yet or exempt from testing)
+status: testing status (tested, not tested, or NA if exempt)
--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(readxl)
-
+library(ggmap)
 
 tested <- read_excel('data-raw/MonthlyPostingMarch.xlsx', skip = 1)
 glimpse(tested)
@@ -25,9 +25,44 @@ tested_schools <- tested %>%
   left_join(lead_present) %>% 
   mutate(status = 'tested', lead = ifelse(is.na(lead), FALSE, lead))
 
+View(tested_schools)
+glimpse(tested_schools)
+tested_schools %>% 
+  separate(schoolAddress, c('Street', 'State', 'Zip'), sep = 'CA', remove = FALSE) %>% View
 un_tested <- read_excel('data-raw/SchoolsUnsampled.xlsx')
 glimpse(un_tested)
 
+not_tested_schools <- un_tested %>% 
+  mutate(schoolAddress = paste(Street, City, 'CA', Zip, sep = ' '),
+         medianResult = NA, unit = NA, lead = NA, status = "not tested") %>% 
+  select(district = District, schoolName = School, schoolAddress, medianResult, 
+         unit, lead, status)
+View(not_tested_schools)
 
 exempt <- read_excel('data-raw/exemption_forms.xlsx')
 glimpse(exempt)
+
+exempt_schools <- exempt %>% 
+  mutate(medianResult = NA, unit = NA, lead = NA, status = "exempt") %>% 
+  select(district = `School District`, schoolName = Name, schoolAddress = Address, 
+         medianResult, unit, lead, status)
+
+View(exempt_schools)
+
+tested_schools %>% 
+  bind_rows(not_tested_schools) %>% 
+  bind_rows(exempt_schools) %>% View()
+
+t <- geocode(tested_schools$schoolAddress[1])
+
+get_address = function(address) {
+  lat_lon = geocode(address)
+  return(tibble(address = address,
+                lon = lat_lon$lon,
+                lat = lat_lon$lat))
+}
+  
+a <- get_address(tested_schools$schoolAddress[1])
+b <- purrr::map2_df(tibble(address=tested_schools$schoolAddress[1:3]), get_address)
+
+
