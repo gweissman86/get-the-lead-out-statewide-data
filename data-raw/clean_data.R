af9b4a1bd9a8891af955d55ec8a333f8213bab91
Sadie Gill <thesadie@gmail.com>
2019-03-19 20:45:22 -0700

    fix district funny names

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readxl)
 library(ggmap)
+library(stringr)
 
 tested <- read_excel('data-raw/MonthlyPostingMarch.xlsx', skip = 1)
 glimpse(tested)
@@ -49,7 +50,63 @@ exempt_schools <- exempt %>%
 
 View(exempt_schools)
 
-tested_schools %>% 
+all_schools <- tested_schools %>% 
   bind_rows(not_tested_schools) %>% 
-  bind_rows(exempt_schools) %>% 
+  bind_rows(exempt_schools)
+
+# geo coding from daniel and victoria
+geo_coded <- read_csv('data-raw/ca_schools_lead_testing_data_geocoded.csv') %>%
+  select(schoolAddress, city, county, latitude, longitude) %>% 
+  unique()
+
+# check for dirty district naming
+dirty_district <- all_schools %>% 
+  select(district) %>% 
+  unique %>% 
+  arrange(district) %>% 
+  separate(district, c('begin', 'rest'), ' ', remove = FALSE) %>% 
+  mutate(prev_beg = lag(begin), next_beg = lead(begin), prev_district = lag(district),
+         next_district = lead(district),
+         match = case_when(
+           begin == prev_beg ~ prev_district,
+           begin == next_beg ~ next_district,
+           TRUE ~ 'no match'
+         )) %>%
+  filter(match != 'no match') %>% 
+  select(district, match) 
+
+# district names from CDE https://www.cde.ca.gov/ds/si/ds/pubschls.asp
+cde_districts <- read_excel('data-raw/pubdistricts.xlsx', skip = 5) %>% 
+  select(district = District) %>% 
+  mutate(in_cde = TRUE)
+
+district_lookup <- dirty_district %>%   
+  left_join(cde_districts) %>% 
+  filter(is.na(in_cde)) %>% 
+  select(-in_cde)
+
+all_schools %>% 
+  left_join(district_lookup) %>% 
+  mutate(district = ifelse(is.na(match), district, match),
+         district = ifelse(is.na(district) | district == 'private', 'Private', district)) %>% 
+  select(-match) %>%
+  left_join(geo_coded) %>% 
   write_csv('ca_schools_lead_testing_data.csv')
+
+# check repeat school names
+repeat_names <- all_schools %>% 
+  select(schoolName, schoolAddress) %>% 
+  unique() %>% 
+  group_by(schoolAddress) %>% 
+  summarise(count = n()) %>% 
+  filter(count > 1) %>% 
+  mutate(duplicate = TRUE) %>% 
+  select(-count) %>% 
+  ungroup()
+
+# didn't see any cases of same school address and similar but different names
+all_schools %>% 
+  left_join(repeat_names) %>% 
+  filter(duplicate) %>% View
+
+
